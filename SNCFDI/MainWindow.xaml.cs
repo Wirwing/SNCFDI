cd9e515e21a1d27224e2776f0f3156b0e7ef6074
Irving <irving.cf@gmail.com>
2014-06-20 12:27:34 -0500

    Se conecta la Ventana Principal con el servicio de lectura Excel.

--- SNCFDI/MainWindow.xaml.cs
+++ SNCFDI/MainWindow.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using Microsoft.Win32;
 using NLog;
-using NPOI.HSSF.UserModel;
-using NPOI.SS.UserModel;
+using SNCFDI.Model;
+using SNCFDI.Service;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SNCFDI
 {
@@ -30,9 +18,19 @@ namespace SNCFDI
 
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
+        private ObservableCollection<Empleado> empleados;
+
+        public ObservableCollection<Empleado> Empleados
+        {
+            get { return empleados; }
+            set { empleados = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            empleados = new ObservableCollection<Empleado>();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -48,71 +46,15 @@ namespace SNCFDI
             // Process open file dialog box results 
             if (result.HasValue && result.Value)
             {
-                List<string> lines = getData(dlg.FileName);
 
-                foreach (string line in lines)
-                {
-                    logger.Info(line);
-                }
+                ExcelReader reader = new ExcelReader(dlg.FileName);
 
-            }
+                List<Empleado> empleadosValid;
+                List<Empleado> empleadosInvalid;
 
-        }
-
-        private List<string> getData(string fileName)
-        {
-
-            FileStream stream = new FileStream(fileName, FileMode.Open);
-            IWorkbook workBook = WorkbookFactory.Create(stream);
-
-            stream.Close();
-
-            ISheet sheet = workBook.GetSheetAt(0);
-
-            IEnumerator rowEnumator = sheet.GetRowEnumerator();
-            IEnumerator cellEnumator;
-
-            List<string> lines = new List<string>();
-            IRow currentRow;
-            ICell currentCell;
-            StringBuilder builder = new StringBuilder();
-
-            while (rowEnumator.MoveNext())
-            {
-
-                currentRow = rowEnumator.Current as IRow;
-
-                //todo: validate row's cell count
-                int cellSize = currentRow.Cells.Count;
-                cellEnumator = currentRow.Cells.GetEnumerator();
-
-                builder.Clear();
-
-                //Extract worker data
-                while (cellEnumator.MoveNext())
-                {
-
-                    currentCell = cellEnumator.Current as ICell;
-
-                    switch (currentCell.CellType)
-                    {
-                        case CellType.String:
-                            builder.Append(currentCell.StringCellValue).Append(' ');
-                            break;
-                        case CellType.Numeric:
-                            builder.Append(currentCell.NumericCellValue.ToString()).Append(' ');
-                            break;
-                        default:
-                            break;
-                    }
-
-                }
-
-                lines.Add(builder.ToString());
+                reader.ParseEmpleados(out empleadosValid, out empleadosInvalid);
 
             }
-
-            return lines;
 
         }
 
